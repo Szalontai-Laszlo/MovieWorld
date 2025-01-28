9d15e03b9d9f931ca6f4fec760e0ae948892c5ed
TothLaszloGabor <lacikaapro@gmail.com>
2025-01-28 21:32:15 +0100

    Home oldalon a "Tovább a filmek" gomb müködése, Adatbázis tábla létrehozása

--- TEENDOK.md
+++ TEENDOK.md
@@ -1,8 +1,8 @@
 css fájlok összevonása ✅
-js fájlok összevonása (controller-ek)
+js fájlok összevonása (controller-ek) ✅
 A Trello részletezése, tesztelési fül létrehozása ✅
 Adatbázis létrehozása
 Regisztráció a két jelszó egyezésének ellenőrzése
-Login, Regisztráció gomb valamit üzenjen ha megnyomom, váltson ablakot
+Login, Regisztráció gomb valamit üzenjen ha megnyomom, váltson ablakot ✅
 Footer kicserélése ✅
 ...
---
+++ db/movieworld.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Jan 28. 21:31
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `movieworld`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `date` date NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
